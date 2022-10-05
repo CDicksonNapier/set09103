d599efc61cb748aeb1680bc12d247b0c227e91d7
Chris Dickson <113544017+CDicksonNapier@users.noreply.github.com>
2022-10-05 22:29:58 +0100

    Add files via upload

---
+++ Chapter 5/flaskRouting.py
@@ -0,0 +1,14 @@
+from flask import Flask 
+app = Flask(__name__)
+@app.route("/")
+def root():
+    return "The default, 'root' route"
+@app.route("/hello/")
+def hello():
+    return "Hello Napier!!! :D"
+@app.route("/goodbye/")
+def goodbye():
+    return "Goodbye Cruel World! :("
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
